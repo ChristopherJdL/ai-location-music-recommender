7c07418719eb0b76a4c2a825d6c34dddafb3df87
christopherjdl <christopher.jdl@outlook.com>
2025-01-30 22:11:31 +0000

    fix: universal script

--- deploy.ps1
+++ deploy.ps1
@@ -2,13 +2,12 @@
 
 
 #pip3 install --target ./package boto3
-if (Select-String -Path "lambda_function.py" -Pattern 'os.getenv("') { Write-Error "os.getenv found in the file."; return }
 
-rm ./my_deployment_package.zip
-Set-Location package
-zip -r ../my_deployment_package.zip .                                       
-Set-Location ..
-zip my_deployment_package.zip lambda_function.py                            
-zip my_deployment_package.zip values.py
+rm ./deployment_package.zip
+cd package
+zip -r ../deployment_package.zip .                                       
+cd ..
+zip deployment_package.zip lambda_function.py                            
+zip deployment_package.zip prompts/*
 
 # aws lambda update-function-code --zip-file ./my_deployment_package.zip --publish
